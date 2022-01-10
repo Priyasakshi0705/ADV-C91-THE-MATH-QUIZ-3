5ce65f12007bb11140b8fd42efa193ae28d1f326
Priyasakshi0705 <85500647+Priyasakshi0705@users.noreply.github.com>
2022-01-10 11:21:56 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,32 @@
+player_1name = localStorage.getItem("player1");
+player_2name = localStorage.getItem("player2");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player_1name + " : ";
+document.getElementById("player2_name").innerHTML = player_2name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("que_turn").innerHTML = player_1name;
+document.getElementById("ans_turn").innerHTML = player_2name;
+
+function send(){
+
+   number1= document.getElementById("number1").value;
+   number2= document.getElementById("number2").value;
+   answer= parseInt(number1) * parseInt(number2);
+
+   question_number = "<h4>" + number1 + " x " + number2 + "</h4>";
+   input_box = "<br>Answer : <input type='number' id='input_check_box'>";
+   check_button= "<br><br><button class ='btn btn-info' onclick='check()'>Check</button>"
+
+   row =question_number + input_box + check_button;
+   document.getElementById("output").innerHTML= row;
+
+   document.getElementById("number1").value ="";
+   document.getElementById("number2").value ="";
+}
+
